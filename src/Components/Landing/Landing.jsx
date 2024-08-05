cf9ccd0fa505a3893288b86e6df469b2f95d9438
Facundo Luna <nfacundoluna@gmail.com>
2024-08-05 17:46:35 -0300

    mejorando la landing

--- src/Components/Landing/Landing.jsx
+++ src/Components/Landing/Landing.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./Landing.css";
 import { IoMdDownload } from "react-icons/io";
 import { IoMenu, IoClose } from "react-icons/io5";
+import { IoChevronDownSharp } from "react-icons/io5";
 
 export const Landing = () => {
   return (
@@ -86,7 +87,19 @@ export const Landing = () => {
           </div>
         </div>
       </section>
-
+      <svg
+        className="wave"
+        version="1.1"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 1440 100"
+        preserveAspectRatio="none"
+      >
+        <path
+          className="wave-path"
+          d="M826.337463,25.5396311 C670.970254,58.655965 603.696181,68.7870267 447.802481,35.1443383 C293.342778,1.81111414 137.33377,1.81111414 0,1.81111414 L0,150 L1920,150 L1920,1.81111414 C1739.53523,-16.6853983 1679.86404,73.1607868 1389.7826,37.4859505 C1099.70117,1.81111414 981.704672,-7.57670281 826.337463,25.5396311 Z"
+          fill="currentColor"
+        ></path>
+      </svg>
       <section className="voice-channel">
       <div className="row container">
           <img src="/section-2.svg" alt="" className="grid-image order-2" />
@@ -101,18 +114,82 @@ export const Landing = () => {
         </div>
       </section>
       <svg
-        class="wave"
+        className="wave wave-inverted"
         version="1.1"
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 1440 100"
         preserveAspectRatio="none"
       >
         <path
-          class="wave-path"
+          className="wave-path"
           d="M826.337463,25.5396311 C670.970254,58.655965 603.696181,68.7870267 447.802481,35.1443383 C293.342778,1.81111414 137.33377,1.81111414 0,1.81111414 L0,150 L1920,150 L1920,1.81111414 C1739.53523,-16.6853983 1679.86404,73.1607868 1389.7826,37.4859505 C1099.70117,1.81111414 981.704672,-7.57670281 826.337463,25.5396311 Z"
           fill="currentColor"
         ></path>
       </svg>
+      <section className="moderation-tools">
+      <div className="row container">
+          <img src="/section-3.svg" alt="" className="grid-image order-2" />
+          <div className="text-group order-1">
+            <h2 className="title secondary-title">
+              From a few to a fandoom
+            </h2>
+            <p className="text-group-description">
+              Get a community of any size running with moderation tools and custom member access. Give members special powers, set up private channels, and more.
+            </p>
+          </div>
+        </div>
+      </section>
+      <svg
+        className="wave "
+        version="1.1"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 1440 100"
+        preserveAspectRatio="none"
+      >
+        <path
+          className="wave-path"
+          d="M826.337463,25.5396311 C670.970254,58.655965 603.696181,68.7870267 447.802481,35.1443383 C293.342778,1.81111414 137.33377,1.81111414 0,1.81111414 L0,150 L1920,150 L1920,1.81111414 C1739.53523,-16.6853983 1679.86404,73.1607868 1389.7826,37.4859505 C1099.70117,1.81111414 981.704672,-7.57670281 826.337463,25.5396311 Z"
+          fill="currentColor"
+        ></path>
+      </svg>
+      <section className="video-call">
+        <div className="row container">
+          <div className="text-group">
+          <h2 className="title secondary-title">
+              Reliable tech for staying close
+            </h2>
+            <p className="text-group-description">
+              Low-latency voice and video feels like you're in the same room. Wave hello over video, watch friends stream their games, or gather up and have a drawing session with screen share.
+            </p>
+          </div>
+          <img src="/section-4.svg" alt="" className="grid-image" />
+
+        </div>
+      </section>
+      <section className="download">
+        <div className="row container remove-grid">
+          <img src="/sparkles.svg" alt="" className="sparkles"/>
+          <h2>Ready to start your journey</h2>
+          <a href="#" className="btn btn-large btn-brand">
+          <IoMdDownload className="btn-download" />
+          Download for Windows
+          </a>
+        </div>
+      </section>
+      <footer className="main-footer">  
+        <div className="row container">
+          <div className="info-boc">
+            <h4 className="footer-title">
+              Your place to talk
+            </h4>
+            <div className="languages">
+              <img src="/flag.png" alt="" />
+              <span>English, USA</span><IoChevronDownSharp />
+            </div>
+            
+          </div>
+        </div>
+      </footer>
     </div>
   );
 };
