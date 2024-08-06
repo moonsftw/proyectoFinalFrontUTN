3e1bbac603e178654bcf6cba78cc15058bc3537f
Facundo Luna <nfacundoluna@gmail.com>
2024-08-06 18:24:57 -0300

    landing cambiada al 100

--- src/Components/Landing/Landing.jsx
+++ src/Components/Landing/Landing.jsx
@@ -4,7 +4,7 @@ import { IoMdDownload } from "react-icons/io";
 import { IoMenu, IoClose } from "react-icons/io5";
 import { IoChevronDownSharp } from "react-icons/io5";
 import { BsTwitterX } from "react-icons/bs";
-import { FaInstagram, FaYoutube } from "react-icons/fa";
+import { FaInstagram, FaYoutube, FaTiktok } from "react-icons/fa";
 import { FaFacebookSquare } from "react-icons/fa";
 
 export const Landing = () => {
@@ -56,26 +56,102 @@ export const Landing = () => {
         </header>
         <div className="row container">
           <div className="hero-content-wrap">
-            <h1 className="title primary-title">Your place to talk</h1>
+            
+            <div className="heroContainer">
+
+            <h1 className="title primary-title">
+              GROUP CHAT THAT'S ALL FUN & GAMES
+            </h1>
             <p className="hero-description">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure
-              odit, exercitationem vero optio eligendi magni maiores corrupti
-              assumenda nemo voluptas, molestiae autem laborum eos officia
-              aspernatur consequuntur quia ipsa incidunt.
+              Discord is great for playing games and chilling with friends, or
+              even building a worldwide community. Customize your own space to
+              talk, play, and hang out.
             </p>
-            <div className="hero-btn-group">
-              <div className="btn btn-large btn-light">
-                <IoMdDownload className="btn-download" />
-                Download for Windows
-              </div>
-              <div className="btn btn-large btn-dark">
-                Open Discord in your browser
-              </div>
+            </div>
+            <img src="664daa37ea162cadf9603500_Art.webp" alt="" />
+          </div>
+          <div className="hero-btn-group">
+            <div className="btn btn-large btn-light">
+              <IoMdDownload className="btn-download" />
+              Download for Windows
+            </div>
+            <div className="btn btn-large btn-dark">
+              Open Discord in your browser
             </div>
           </div>
         </div>
       </section>
-      <section className="collaboration">
+      <div className="container-probando ">
+        <section className="probando">
+          <div className="probando2 order-2">
+            <div className="video">
+              <video autoPlay loop muted playsInline data-wf-ignore="true">
+                <source
+                  src="/663b271d6f05c8c9e11f8d65_Discord Refresh Sound-MP4-transcode.mp4"
+                  data-wf-ignore="true"
+                />
+              </video>
+            </div>
+          </div>
+          <div className="text-probando">
+            <h2 className="title-probando">MAKE YOUR GROUP CHATS MORE FUN</h2>
+            <p className="parrafo-probando">
+              Use custom emoji, stickers, soundboard effects and more to add
+              your personality to your voice, video, or text chat. Set your
+              avatar and a custom status, and write your own profile to show up
+              in chat your way.
+            </p>
+          </div>
+        </section>
+      </div>
+      <div className="container-probando">
+        <section className="probando">
+          <div className="probando2">
+            <div className="video">
+              <video autoPlay loop muted playsInline data-wf-ignore="true">
+                <source
+                  src="https://cdn.prod.website-files.com/6257adef93867e50d84d30e2%2F665434315cbc60da2d4c9684_Discord_Website_Refresh_Same%20Room_EN_V2-transcode.mp4"
+                  data-wf-ignore="true"
+                />
+              </video>
+            </div>
+          </div>
+          <div className="text-probando">
+            <h2 className="title-probando">MAKE YOUR GROUP CHATS MORE FUN</h2>
+            <p className="parrafo-probando">
+              Use custom emoji, stickers, soundboard effects and more to add
+              your personality to your voice, video, or text chat. Set your
+              avatar and a custom status, and write your own profile to show up
+              in chat your way.
+            </p>
+          </div>
+        </section>
+      </div>
+      <div className="container-probando">
+        <section className="probando">
+          <div className="probando2 order-2">
+            <div className="video">
+              <video autoPlay loop muted playsInline data-wf-ignore="true">
+                <source
+                  src="https://cdn.prod.website-files.com/6257adef93867e50d84d30e2/6638c6b04eff56a99c1e2d7d_Discord_Website_Refresh_Hop-In-transcode.mp4"
+                  data-wf-ignore="true"
+                />
+              </video>
+            </div>
+          </div>
+          <div className="text-probando">
+            <h2 className="title-probando">MAKE YOUR GROUP CHATS MORE FUN</h2>
+            <p className="parrafo-probando">
+              Use custom emoji, stickers, soundboard effects and more to add
+              your personality to your voice, video, or text chat. Set your
+              avatar and a custom status, and write your own profile to show up
+              in chat your way.
+            </p>
+          </div>
+        </section>
+      </div>
+
+      {/*  <section className="collaboration">
         <div className="row container">
           <img src="/section-1.svg" alt="" className="grid-image" />
           <div className="text-group">
@@ -169,24 +245,30 @@ export const Landing = () => {
           </div>
           <img src="/section-4.svg" alt="" className="grid-image" />
         </div>
-      </section>
+      </section> */}
       <section className="download">
         <div className="row container remove-grid">
-          <img src="/sparkles.svg" alt="" className="sparkles" />
+          {/*  <img src="/sparkles.svg" alt="" className="sparkles" />
           <h2>Ready to start your journey?</h2>
           <a href="#" className="btn btn-large btn-brand">
             <IoMdDownload className="btn-download" />
             Download for Windows
+          </a> */}
+          <h2>YOU CAN'T SCROLL ANIMORE. BETTER GO CHAT</h2>
+          <a href="#" className="btn btn-large btn-brand">
+            <IoMdDownload className="btn-download" />
+            Download for Windows
           </a>
+          <img src="/lastBanner.webp" alt="" />
         </div>
       </section>
       <footer className="main-footer">
         <div className="row container">
-          <div className="info-boc">
-            <h4 className="footer-title">Your place to talk</h4>
+          <div className="info-box">
+            
             <div className="languages">
               <img src="/flag.png" alt="" />
-              <span>English, USA</span>
+              <span>English</span>
               <IoChevronDownSharp />
             </div>
             <div className="social-media">
@@ -195,7 +277,6 @@ export const Landing = () => {
               </a>
               <a href="#" className="sm-link">
                 <FaInstagram />
-                
               </a>
               <a href="#" className="sm-link">
                 <FaFacebookSquare />
@@ -203,8 +284,118 @@ export const Landing = () => {
               <a href="#" className="sm-link">
                 <FaYoutube />
               </a>
+              <a href="#" className="sm-link">
+                <FaTiktok />
+              </a>
             </div>
           </div>
+          <div className="spacer"></div>
+          <div className="footer-links-group">
+            <h5 className="footer-title">Product</h5>
+            <a href="#" className="footer-link">
+              Download
+            </a>
+            <a href="#" className="footer-link">
+              Nitro
+            </a>
+            <a href="#" className="footer-link">
+              Status
+            </a>
+            <a href="#" className="footer-link">
+              App Directory
+            </a>
+            <a href="#" className="footer-link">
+              New Mobile Experience
+            </a>
+          </div>
+          <div className="footer-links-group">
+            <h5 className="footer-title">Company</h5>
+            <a href="#" className="footer-link">
+              About
+            </a>
+            <a href="#" className="footer-link">
+              Jobs
+            </a>
+            <a href="#" className="footer-link">
+              Brand
+            </a>
+            <a href="#" className="footer-link">
+              Newsroom
+            </a>
+          </div>
+
+          <div className="footer-links-group">
+            <h5 className="footer-title">Resources</h5>
+            <a href="#" className="footer-link">
+              College
+            </a>
+            <a href="#" className="footer-link">
+              Support
+            </a>
+            <a href="#" className="footer-link">
+              Safety
+            </a>
+            <a href="#" className="footer-link">
+              Blog
+            </a>
+            <a href="#" className="footer-link">
+              Feedback
+            </a>
+            <a href="#" className="footer-link">
+              StreamKit
+            </a>
+            <a href="#" className="footer-link">
+              Creators
+            </a>
+            <a href="#" className="footer-link">
+              Community
+            </a>
+            <a href="#" className="footer-link">
+              Developers
+            </a>
+            <a href="#" className="footer-link">
+              Gaming
+            </a>
+            <a href="#" className="footer-link">
+              Quests
+            </a>
+            <a href="#" className="footer-link">
+              Official 3rd Party Merch
+            </a>
+          </div>
+          <div className="footer-links-group">
+            <h5 className="footer-title">Policies</h5>
+            <a href="#" className="footer-link">
+              Terms
+            </a>
+            <a href="#" className="footer-link">
+              Privacy
+            </a>
+            <a href="#" className="footer-link">
+              Cookie Settings
+            </a>
+            <a href="#" className="footer-link">
+              Guideliness
+            </a>
+            <a href="#" className="footer-link">
+              Acknowledgements
+            </a>
+            <a href="#" className="footer-link">
+              Licenses
+            </a>
+            <a href="#" className="footer-link">
+              Company Information
+            </a>
+          </div>
+          <div className="divider"></div>
+        </div>
+        <div className="row container remove-grid">
+          <a href="#" className="logo">
+            <img src="/logo.svg" alt="" />
+          </a>
+          <a href="#" className="btn btn-small btn-brand">
+            Login
+          </a>
         </div>
       </footer>
     </div>
