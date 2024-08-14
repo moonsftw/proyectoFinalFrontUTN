3cd160e4fc36b540328693ca3b82a42c9d2f0d91
Facundo Luna <nfacundoluna@gmail.com>
2024-08-14 18:53:06 -0300

    aprendiendo a usar el position absolute en comunidad.jsx

--- src/Pages/Comunidad/Comunidad.jsx
+++ src/Pages/Comunidad/Comunidad.jsx
@@ -6,14 +6,67 @@ const Comunidad = () => {
   return (
     <>
       <main className="comunidad-container">
-        <section className="comunidad-1"></section>
-        <Header className="main-header" />
-        <section className="comunidad-2"></section>
-        <div className="comunidad-banner">
-          <img src="/servers/banners/midjourney.jpg" alt="" />
-        </div>
-        <Footer className="found"></Footer>
+        <section className="comunidad-section-1">
+          <Header className='main-header' />
+        </section>
+        <section className="comunidad-section-2">
+
+        </section>
+        <section className="comunidad-section-3">
+
+        </section>
+        <section className="comunidad-container-banner">
+
+          <img className="comunidad-banner" src="/servers/banners/midjourney.jpg" alt="" />
+          <img className="comunidad-logo" src="/servers/logos/midjourney.jpg" alt="" />
+
+        </section>
+        <section className="comunidad-section-4">
+          <div>
+            <h1>Titulo</h1>
+            <p>Description</p>
+            <span>online suscriptores</span>
+            <div>
+              tabla
+            </div>
+            <h3>About</h3>
+            <p>about description</p>
+            <h2>Supported Languages</h2>
+            <p>English</p>
+            <h1>Titulo</h1>
+            <p>Description</p>
+            <span>online suscriptores</span>
+            <div>
+              tabla
+            </div>
+            <h3>About</h3>
+            <p>about description</p>
+            <h2>Supported Languages</h2>
+            <p>English</p>
+            <h1>Titulo</h1>
+            <p>Description</p>
+            <span>online suscriptores</span>
+            <div>
+              tabla
+            </div>
+            <h3>About</h3>
+            <p>about description</p>
+            <h2>Supported Languages</h2>
+            <p>English</p>
+            <h1>Titulo</h1>
+            <p>Description</p>
+            <span>online suscriptores</span>
+            <div>
+              tabla
+            </div>
+            <h3>About</h3>
+            <p>about description</p>
+            <h2>Supported Languages</h2>
+            <p>English</p>
+          </div>
+        </section>
       </main>
+      <Footer className='found' />
     </>
   );
 };
